2975f2eaca1d444afa34e4f3649b39dbdddf3a9c
Ashik cv <ashikcv17@gmail.com>
2022-07-09 23:16:29 +0530

    changes in testmonial file

--- src/model/testmonial.js
+++ src/model/testmonial.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose')
-const uri = process.env.MONGODB_URI;
+// const uri = process.env.MONGODB_URI;
 
-mongoose.connect(uri)
-.then((res)=>{
-    console.log('database connected successfuly');
+// mongoose.connect(uri)
+// .then((res)=>{
+//     console.log('database connected successfuly');
+// }).catch((err)=>{
+//     console.log('an error occured'+err);
+// })
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }).then(()=>{
+    console.log("Database Connection Successful...")
 }).catch((err)=>{
-    console.log('an error occured'+err);
-})
+ console.log(err)
+});
 
-const testmoSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+// const testmoSchema = new mongoose.Schema({
+    var testmoSchema = new Schema({
 
     person:String,
     role:String,
@@ -16,6 +24,6 @@ const testmoSchema = new mongoose.Schema({
     image:String
 })
 
-const testModel =mongoose.model('test',testmoSchema)
+const testModel = mongoose.model('test',testmoSchema)
 
 module.exports=testModel
